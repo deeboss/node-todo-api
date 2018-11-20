8b5ac07ac06374a029b55aece6391fbc12f82207
Don <hello@donbos.co>
2018-11-20 19:20:25 +0800

    Reconfiguring due to heroku needing CC

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,21 @@
 const mongoose = require('mongoose');
 
+// let db = {
+// 	localhost: 'mongodb://127.0.0.1:27017/TodoApp',
+//   mlab: 'mongodb://don:1qwerty7@ds119548.mlab.com:19548/mongoose-for-mongodb-nodejs'
+// };
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp');
+// mongoose.connect(db.localhost || db.mlab) ;
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp') ;
 
 
 module.exports = {mongoose}
+
+
+// let db = {
+//   localhost: 'mongodb://localhost:27017/TodoApp',
+//   mlab: 'mongodb://<user>:<pass>@ds149268.mlab.com:49268/todoapp'
+// };
+// mongoose.connect( db.localhost || db.mlab);
