774c7d9339b7d152a3a5fa7e55d49f33783eb8c2
Don <hello@donbos.co>
2018-11-20 17:19:32 +0800

    Setup App for Heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {Users} = require('./models/user');
 
+const port = process.env.PORT || 3000;
 
 let app = express();
 
@@ -61,8 +62,8 @@ app.get('/todos/:id', (req, res) => {
 
 }) 
 
-app.listen('3000', () => {
-	console.log("Listening at port 3000");
+app.listen(port, () => {
+	console.log(`Listening at ${port}`);
 });
 
 
