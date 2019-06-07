e4213d7e2523e40c9302974cb6b3b22beeaa8f14
Don <hello@donbos.co>
2019-06-07 23:35:46 +0800

    replacing promises with async / await

--- server/server.js
+++ server/server.js
@@ -72,40 +72,6 @@ app.get('/todos/:id', authenticate, (req, res) => {
 
 // DELETE /todo/1234
 
-// OLD - Promises and Callback chaining
-// app.delete('/todos/:id', authenticate, (req, res) => {
-// 	let id = req.params.id;
-
-// 	// Validate ID, if not valid then return 404
-// 	if (!ObjectID.isValid(id)) {
-// 		return res.status(404).send("Sorry, your ID was not valid");
-// 	}
-
-// 	// If valid, remove todo by ID
-// 	// Success
-// 	Todo.findOneAndRemove({
-// 		_id: id,
-// 		_creator: req.user._id
-// 	}).then((todo) => {
-// 		// If no doc, send 404
-// 		if (!todo) {
-// 			return res.status(404).send("Your todo item does not exist!");
-// 		}
-
-// 		// If doc, send doc back with 200
-// 		res.send({todo});
-
-
-// 	}).catch((e) => {
-// 		// Error
-// 			// 400
-// 			// Empty body
-// 		res.status(400).send();
-// 	})
-
-
-// });
-
 
 // NEW - Async and Await
 app.delete('/todos/:id', authenticate, async (req, res) => {
@@ -185,21 +151,6 @@ app.patch('/todos/:id', authenticate, (req, res) => {
 // Most similar is to create a new route for todo
 // Pick off the properties, email and password (and gives body variable to pass in)
 
-// OLD - Promises and Callback chaining
-// app.post('/users', (req, res) => {
-// 	let body = _.pick(req.body, ['email', 'password']);
-// 	let user = new User(body);
-
-// 	user.save().then(() => {
-// 		return user.generateAuthToken();
-// 	}).then((token) => {
-// 		res.header('x-auth', token).send(user);
-// 	}).catch((e) => {
-// 		res.status(400).send(e);
-// 	});
-
-// });
-
 // NEW - Async and Await
 app.post('/users', async (req, res) => {
 	try {
@@ -217,19 +168,6 @@ app.get('/users/me', authenticate, (req, res) => {
 	res.send(req.user);
 });
 
-// OLD - Promises and Callback chaining
-// app.post('/users/login', (req,res) => {
-// 	let body = _.pick(req.body, ['email', 'password']);
-
-// 	User.findByCredentials(body.email, body.password).then((user) => {
-// 		return user.generateAuthToken().then((token) => {
-// 			res.header('x-auth', token).send(user);
-// 		});
-// 	}).catch((e) => {
-// 		res.status(400).send();
-// 	})
-
-// });
 
 // NEW - Async and Await
 app.post('/users/login', async (req,res) => {
@@ -243,15 +181,6 @@ app.post('/users/login', async (req,res) => {
 	}
 });
 
-
-// OLD - Promises and Callback chaining
-// app.delete('/users/me/token', authenticate, (req, res) => {
-// 	req.user.removeToken(req.token).then(() => {
-// 		res.status(200).send();
-// 	}, () => {
-// 		res.status(400).send();
-// 	});
-// });
 
 // NEW - Async and Await
 app.delete('/users/me/token', authenticate, async (req, res) => {
